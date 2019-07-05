9e8ba87c380f6c0f5adaf7878e190383c3b53d33
Lara Sanders <larasanders@gmail.com>
2019-07-05 18:27:14 +0000

    Automatically backed up by Learn

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,27 @@
-// TODO: Create action creators as defined in tests
+export const addQuote = quote => {
+  return {
+    type: 'ADD_QUOTE',
+    author
+  };
+};
+
+export const removequote = id => {
+  return {
+    type: 'REMOVE_QUOTE',
+    id
+  };
+};
+
+export const upvoteQuote = quote => {
+  return {
+    type: 'UPVOTE_QUOTE',
+    book
+  };
+};
+
+export const downvoteQuote= quote => {
+  return {
+    type: 'DOWNVOTE_QUOTE',
+    id
+  };
+};
